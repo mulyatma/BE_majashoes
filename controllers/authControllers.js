604c0b206dc740178cfed8768435e0d0091b5323
Mulya Tri Atmaja <mulyaatmaja604@gmail.com>
2024-06-11 23:18:50 +0700

    change status

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -13,7 +13,7 @@ module.exports = {
 
         try {
             await newUser.save()
-            res.status(200).json({ message: "User successfully created" })
+            res.status(201).json({ message: "User successfully created" })
         } catch (err) {
             res.status(500).json({ message: `${err}` })
         }
